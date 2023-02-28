c3f4aede8e67c1b04e060c422bd6f6c4320e68cb
dev <ecomobile.vn@gmail.com>
2023-02-28 08:54:43 -0800

    1.0.2.2-alpha

--- datastore/src/main/java/com/datastore/BaseApplication.kt
+++ datastore/src/main/java/com/datastore/BaseApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDex
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
@@ -17,6 +19,9 @@ abstract class BaseApplication : Application() {
             koinModules.addAll(addKoinModules())
             modules(koinModules)
         }
+        val builder = RequestConfiguration.Builder()
+        builder.setTestDeviceIds(addDeviceTest())
+        MobileAds.setRequestConfiguration(builder.build())
     }
 
     override fun attachBaseContext(base: Context?) {
@@ -25,6 +30,10 @@ abstract class BaseApplication : Application() {
     }
 
     open fun addKoinModules(): MutableList<Module> {
+        return mutableListOf()
+    }
+
+    open fun addDeviceTest(): MutableList<String> {
         return mutableListOf()
     }
 }
